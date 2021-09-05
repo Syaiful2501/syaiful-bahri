abea5777099e0ad119a9469ff17ff2326812535c
Syaiful2501 <90129814+Syaiful2501@users.noreply.github.com>
2021-09-05 14:56:21 +0700

    Add files via upload

---
+++ suhu.py
@@ -0,0 +1,9 @@
+print("Konverensi suhu Celcius ke Fahrenheit,Reamur,Dan Kelvin")
+c = int(input("Masukkan nilai Celcius: "))
+f = c*9/5+32
+r = 4/5*c
+k = c+273
+print("Nilai Suhu Dalam Fahrenheit: ",f)
+print("Nilai Suhu Dalam Reamur: ",r)
+print("Nilai suhu Dalam Kelvin: ",k)
+print(" ")
